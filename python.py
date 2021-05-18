a378dab5577e0541d1a0d9a2a18d951148f896b3
Bruno-Ferreira-7 <64493754+Bruno-Ferreira-7@users.noreply.github.com>
2021-05-19 00:52:03 +0100

    Update python.py

--- python.py
+++ python.py
@@ -9,7 +9,7 @@ from spacy_transformers.pipeline_component import DEFAULT_CONFIG
 
 DEFAULT_TEXT = ""
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_trf")
 spacy_model = "en_core_web_trf"
 
 st.title("Identifyer")
